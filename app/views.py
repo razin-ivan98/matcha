025e8d8282daa8cdd4e943ea8b5a749dad98d5fa
Chaurus Orange dartwing <chorange@at-h2.21-school.ru>
2019-10-07 16:06:11 +0300

    second

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
-@app.route('/')
 
+@app.route('/')
 @app.route('/index')
 def index():
     user = { 'nickname': 'Miguel' } # выдуманный пользователь
@@ -22,17 +22,17 @@ def index():
         }
     ]
     return render_template("index.html",
-        user = user,
-        posts = posts)
+                           user=user,
+                           posts=posts)
 
 
-@app.route('/login', methods= ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="'+form.openid.data+
-            '", remember_me='+str(form.remember_me.data))
+              '", remember_me='+str(form.remember_me.data))
         return redirect('/index')
     return render_template('login.html',
-        title = 'Sign In',
-        form = form)
+                           title='Sign In',
+                           form=form)
---
+++ start.sh
@@ -0,0 +1,24 @@
+clear
+
+git clone https://github.com/razin-ivan98/matcha.git
+
+docker-machine stop
+docker-machine rm default
+
+rm -rf ~/goinfre/.docker
+rm -rf ~/.docker
+
+mkdir ~/goinfre/.docker
+ln -s ~/goinfre/.docker ~/.docker
+
+docker-machine create -d virtualbox default
+
+eval $(docker-machine env)
+
+docker run --name matcha_mysql -p 3306:3306 -d -v matcha_db:/var/lib/mysql --restart on-failure:5 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=base mysql:5 --default-authentication-plugin=mysql_native_password
+docker run --name matcha_phpmyadmin -d --link matcha_mysql:db -p 8081:80 phpmyadmin/phpmyadmin
+
+echo "docker-machine:"
+docker-machine ls | grep -E "tcp" | awk -F' ' '{print $5}' | awk -Ftcp:// '{print $2}' | awk -F: '{print $1}'
+echo "mysql:"
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' matcha_mysql
