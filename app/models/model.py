4eae766c9ef81ab0aeeed7ced9bfc9991cfaffee
razin-ivan98 <razin-ivan98@ya.ru>
2020-09-01 11:03:33 +0300

    py-dependencies

--- app/models/model.py
+++ app/models/model.py
@@ -6,11 +6,11 @@ from flask import Flask
 class Model():
     def __init__(self):
         self.db = mysql.connector.connect(
-           # host='192.168.99.140',
-           host='172.17.0.2',
+        #    host='172.17.0.2',
+           host='localhost',
             user='root',
-            # passwd='root',
-            passwd='my-secret-pw',
+            passwd='root',
+            # passwd='my-secret-pw',
             database='matcha_db',
             charset='utf8mb4',
             collation='utf8mb4_unicode_ci',
